ec7cee6a34a4d6f47343128defa0c7af945abeab
shreenath <khandekarshreenath23@gmail.com>
2022-02-01 21:57:51 +0530

    added albums component

--- albums/src/Components/Albums.js
+++ albums/src/Components/Albums.js
@@ -3,11 +3,11 @@ import Album from "./Album.js";
 
 export default function Albums(props){
     return props.albums.map((album) => (
-         <Album
+          <Album
           key={album.id}
           album={album.album}
           artist={album.artist}
           albumCover={album.albumCover}
-         />
+          />
       ));
 } 
